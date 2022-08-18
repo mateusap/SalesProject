0f159051f4d358cbb873e483fc3a86e48a98982c
Mateus Silva <ma.aparecido.silva@hotmail.com>
2022-08-18 16:01:28 -0300

    Criado banco de dados com CRUD e integração do projeto

---
+++ SalesProject/Data/SalesProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesProject.Models;
+
+namespace SalesProject.Data
+{
+    public class SalesProjectContext : DbContext
+    {
+        public SalesProjectContext (DbContextOptions<SalesProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesProject.Models.Department> Department { get; set; }
+    }
+}
--- SalesProject/Startup.cs
+++ SalesProject/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesProject.Data;
 
 namespace SalesProject
 {
@@ -33,6 +35,10 @@ namespace SalesProject
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesProjectContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("SalesProjectContext"), builder =>
+                        builder.MigrationsAssembly("SalesProject")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
